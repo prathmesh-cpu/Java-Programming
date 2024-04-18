a61e9d34a89dc159515babea2cb9fc6a74e61225
Prathamesh Dhavale <dhawaleprathmesh2003@gmail.com>
2024-04-18 22:18:37 +0530

    Update Recursion Basics

--- 19. Recursion Basics/_2powerOfNum.java
+++ 19. Recursion Basics/_2powerOfNum.java
@@ -5,8 +5,8 @@ public class _2powerOfNum {
     // Find xⁿ --------------->
     // Time Complexity: O(n)
     public static long power(int x, int n) {
-        if (n == 1) {
-            return x;
+        if (n == 0) {
+            return 1;
         }
         return x * power(x, n - 1);
     }
@@ -14,8 +14,8 @@ public class _2powerOfNum {
     // Find xⁿ in O(logn) --------------->
     // Time Complexity: O(logn)
     public static long optimizedPower(int x, int n) {
-        if (n == 1) {
-            return x;
+        if (n == 0) {
+            return 1;
         }
         long halfPower = optimizedPower(x, n / 2);
         if ((n & 1) == 0) {
@@ -27,12 +27,12 @@ public class _2powerOfNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x = 3, n = 20;
+        int x = 3, n = 3;
         System.out.println(x + "^" + n + " = " + power(x, n));
         System.out.println();
 
         x = 2;
-        n = 10;
+        n = 5;
         System.out.println(x + "^" + n + " (Optimized) = " + optimizedPower(x, n));
 
         sc.close();
